30b9570b6f729e09280dda2e931d5f1e5f2eab22
Bruce MacDonald <macdonb@nationwide.com>
2015-08-22 17:42:03 -0700

    fixed error
    Coursera R Programming Course Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -10,27 +10,27 @@ makeCacheMatrix <- function(x = matrix()) {
     x <<- y
     m <<- NULL
   }
-  get<-funtion() x
-  setinverse <-function(inverse) m <<- inverse
-  getinverse <- function() m
+  get<-function() x
+  setSolve <-function(solve) m <<- solve
+  getSolve <- function() m
   list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
-## This function will look for a calculated value in an environment
+## This function will look for a calculated inverse value in an environment
 ## and create one if not found that will get cached for future calls.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m <- x$getinverse()
+  m <- x$getSolve()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
   data <- x$get()
-  m <- inverse(data, ...)
-  x$setinverse(m)
+  m <- solve(data, ...)
+  x$setSolve(m)
   m
 }
